b73f1811b5f773ed161f3c8b21b9a9f4028a197f
akka-tech <akkaabdlwahd3@gmail.com>
2025-08-03 13:31:06 +0200

    add

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -9,3 +9,27 @@ def create_notification_on_message(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import Message, MessageHistory
+
+@receiver(pre_save, sender=Message)
+def log_old_content_before_update(sender, instance, **kwargs):
+    if not instance.pk:
+        # This is a new message, no old content to log
+        return
+
+    try:
+        old_message = Message.objects.get(pk=instance.pk)
+    except Message.DoesNotExist:
+        # The message does not exist in DB yet
+        return
+
+    if old_message.content != instance.content:
+        # Log the old content before update
+        MessageHistory.objects.create(
+            message=instance,
+            old_content=old_message.content
+        )
+        instance.edited = True
