45975b13b9d2b0320fee8472ed3a12b284e745ac
akka-tech <akkaabdlwahd3@gmail.com>
2025-08-03 15:44:14 +0200

    add

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Message(models.Model):
+    unread = UnreadMessagesManager()
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
